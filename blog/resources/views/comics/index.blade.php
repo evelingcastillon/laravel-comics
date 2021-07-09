b9a5b64556c4e8ab181b18d36f16d366f64850f2
evelingcastillon <evelingcastillon0304@gmail.com>
2021-07-09 21:34:36 +0200

    aggiunto comics con css

--- blog/resources/views/comics/index.blade.php
+++ blog/resources/views/comics/index.blade.php
@@ -3,14 +3,25 @@
 @section('title', 'Comic DC Comics')
 
 @section('content')
-<h1>Comic</h1>
-<div class="box_comic">
-    @foreach ($comics as $index => $comic)
-    <a href="{{route('comic', ['id' => $index])}}">
-        <img src="{{$comic['thumb']}}" alt="">
-        <p>{{ $comic['series']}}</p>
-    </a>
-    @endforeach
+<div class="container">
+
+    <h2 class="title_comics">CURRENT SERIES</h2>
+    <div class="box_comic">
+        @foreach ($comics as $index => $comic)
+        <a href="{{route('comic', ['id' => $index])}}">
+            <div class="img_comics">
+                <img src="{{$comic['thumb']}}" alt="">
+            </div>
+            <p>{{ $comic['series']}}</p>
+        </a>
+        @endforeach
+    </div>
+
+    <div class="btn_lead_more">
+        <a href="#">
+            LEAD MORE
+        </a>
+    </div>
 </div>
 
 @endsection
