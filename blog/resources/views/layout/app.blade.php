d65d104e2fc0366f4a5328ae7fba465b6562ed60
evelingcastillon <evelingcastillon0304@gmail.com>
2021-07-09 17:58:24 +0200

    aggiunto la navbar

--- blog/resources/views/layout/app.blade.php
+++ blog/resources/views/layout/app.blade.php
@@ -14,18 +14,32 @@
     </head>
     <body>
         <header id="site_header">
-            <nav class="menu">
-                <a href="{{route('characters')}}">CHARACTERS</a>
-                <a href="{{route('home')}}">COMICS</a>
-                <a href="{{route('movies')}}">MOVIES</a>
-                <a href="{{route('tv')}}">TV</a>
-                <a href="{{route('games')}}">GAMES</a>
-                <a href="{{route('collectibles')}}">COLLECTIBLES</a>
-                <a href="{{route('videos')}}">VIDEOS</a>
-                <a href="{{route('fans')}}">FANS</a>
-                <a href="{{route('news')}}">NEWS</a>
-                <a href="{{route('shop')}}">SHOP</a>
-            </nav>
+
+            <div class="primary_menu">
+                
+            </div>
+
+            <div class="secondary_menu container">
+
+                <a href="{{route('home')}}" class="logo">
+                    <img src="{{asset('img/dc-logo.png')}}" alt="">
+                </a>
+                <nav class="main_menu">
+                    <a href="{{route('characters')}}">CHARACTERS</a>
+                    <a href="{{route('comics')}}">COMICS</a>
+                    <a href="{{route('movies')}}">MOVIES</a>
+                    <a href="{{route('tv')}}">TV</a>
+                    <a href="{{route('games')}}">GAMES</a>
+                    <a href="{{route('collectibles')}}">COLLECTIBLES</a>
+                    <a href="{{route('videos')}}">VIDEOS</a>
+                    <a href="{{route('fans')}}">FANS</a>
+                    <a href="{{route('news')}}">NEWS</a>
+                    <a href="{{route('shop')}}">SHOP</a>
+                </nav>
+                <div class="search">
+                    <input type="text" name="" id="" placeholder="Search">
+                </div>
+            </div>
         </header>
         <main id="site_main">
         @yield('content')
