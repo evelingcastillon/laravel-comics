a8849714499f998357289647e5ee8c815daf5301
evelingcastillon <evelingcastillon0304@gmail.com>
2021-07-09 07:47:20 +0200

    collegamento con layout e root

---
+++ blog/resources/views/layout/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('page_title', 'Dc Comics')</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <header id="site_header"></header>
+        <main id="site_main">
+        @yield('content')
+        </main>
+        <footer id="site_footer"></footer>
+    </body>
+</html>
